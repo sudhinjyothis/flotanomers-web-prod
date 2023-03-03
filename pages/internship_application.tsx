9ae3c0e75f31b3858e16f7b2e5d821d0f465bc72
Sunidhi Patwardhan <patwardhansunidhi1377@gmail.com>
2023-03-03 20:43:20 +0530

    Updated

---
+++ pages/internship_application.tsx
@@ -0,0 +1,133 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import Layout from "../src/layout/Layout";
+import PageBanner from "../src/layout/PageBanner";
+export default function Application() {
+  return (
+    <Layout header={1} className="home-one" footer={1} noHeader={undefined}>
+        <PageBanner titleHtml={`Internship Application`} titleText="Internship Application" />
+        <div className="application_form">
+            <div className="contact">
+                <div className="application_title">Internship Application</div>
+                <div className="contact_details">
+                    <div>Contact Person: Sharun Joseph</div>
+                    <div>Contact Number: 8242988967</div>
+                    <div>If you have any queries, <a href="https://www.flotanomers.com/contact">contact us</a></div>
+                </div>
+            </div>
+            <div className="intern_form">
+                <form action="">
+                    <div className="complete_form">
+                        <div className="form1">
+                            <label className="form_text">Name</label>
+                            <input type="text" required/><br />
+
+                            <label className="form_text">Address</label>
+                            <input type="text" required/><br />
+
+                            <label className="form_text">Date of Join</label>
+                            <input type="date" required/><br />
+
+                            <label className="form_text">Mobile Number</label>
+                            <input type="tel" required/><br />
+                            
+                            <p>Choose your Field Of Interest</p>
+                            
+                            <input type="radio" name="interest" required/>
+                            <label className="label_text">UAV Pilot</label><br />
+
+                            <input type="radio" name="interest"/>
+                            <label className="label_text">GIS Officer</label><br />
+
+                            <input type="radio" name="interest"/>
+                            <label className="label_text">WASP Development</label><br />
+
+                            <input type="radio" name="interest"/>
+                            <label className="label_text">Training Manager</label><br />
+
+                            <input type="radio" name="interest"/>
+                            <label className="label_text">Training Associates</label><br />
+
+                            <input type="radio" name="interest"/>
+                            <label className="label_text">Technical Officer</label><br />
+
+                            <input type="radio" name="interest"/>
+                            <label className="label_text">Technical Content Writer</label><br />
+
+                            <input type="radio" name="interest"/>
+                            <label className="label_text">Social Media Manager</label><br />
+
+                            <input type="radio" name="interest"/>
+                            <label className="label_text">OutReach Officer</label><br />
+
+                            <input type="radio" name="interest"/>
+                            <label className="label_text">Operations Officer</label><br />
+
+                            <input type="radio" name="interest"/>
+                            <label className="label_text">Office Executive</label><br />
+
+                            <input type="radio" name="interest"/>
+                            <label className="label_text">MultiFlot Development</label><br />
+
+                            <input type="radio" name="interest"/>
+                            <label className="label_text">GIS Analyst</label><br />
+
+                            <input type="radio" name="interest"/>
+                            <label className="label_text">Flotaron Development</label><br />
+
+                            <input type="radio" name="interest"/>
+                            <label className="label_text">Customer Support</label><br />
+
+                            <input type="radio" name="interest"/>
+                            <label className="label_text">Cruise Copter Development</label><br />
+
+                            <input type="radio" name="interest"/>
+                            <label className="form_text">Content Creator</label><br />
+
+                            <input type="radio" name="interest"/>
+                            <label className="label_text">AirDoe Development</label><br />
+
+                            <input type="radio" name="interest"/>
+                            <label className="label_text">AgroFlot Development</label><br />
+
+                            <input type="radio" name="interest"/>
+                            <label className="label_text">Inventory Manager</label><br />
+
+                        </div>
+                        <div className="form2">
+                            <label>Email</label>
+                            <input type="email" required/><br />
+
+                            <label>Projects (For multiple projects separate it with commas)</label>
+                            <input type="text" required/><br />
+
+                            <label>Batch of year</label>
+                            <input type="text" required/><br />
+
+                            <label>LinkedIn URL</label>
+                            <input type="url" required/><br />
+                            
+                            <label>Aadhar Card</label>
+                            <input type="file" required/><br />
+
+                            <label>PAN Card</label>
+                            <input type="file" required/><br />
+
+                            <label>Latest Resume</label>
+                            <input type="file" required/><br />
+
+                            <label>Professional Display Picture</label>
+                            <input type="file" required/><br />
+                        </div>
+                        
+                        
+                    </div>
+                </form>
+            </div>
+            <div className="submit_button">
+                <button>Submit Application</button>
+            </div>
+        </div>
+    </Layout>   
+  );
+}
---
+++ pages/products.tsx
@@ -0,0 +1,70 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import Layout from "../src/layout/Layout";
+import PageBanner from "../src/layout/PageBanner";
+export default function Prodcuts() {
+  return (
+    <Layout  header={1} className="home-one" footer={1} noHeader={undefined}>
+        <PageBanner titleHtml={`Products`} titleText="Products" />
+        <div className="container_main">
+            <div className="text_about">We provide aerial solutions for hobby, GIS and Agricultural sectors. The products are indigenously designed and custom assembled.</div>
+            <div className="container_box1">
+                <img src="assets/images/products/airdoe.png" alt="airdoe" />
+                <div className="container_text">
+                    <div className="text_airdoe">Airdoe</div>
+                    <div className="text_paragraph1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+                    <div className="text_spec">Specifications</div>
+                    <div className="text_speclist">
+                        <ul>
+                            <li>Lightweight fixed wing plane</li>
+                            <li>Weighs less than 150g </li>
+                            <li>Coreless motors and modular components </li>
+                            <li>Android app-based control</li>
+                        </ul>
+                    </div>
+                    <div className="btnbuy">
+                        <button>Buy Now</button>
+                    </div>
+                </div>
+                
+            </div>
+            <div className="container_box2">
+
+                <div className="container_outerbox1">
+                    <div className="container_img2">
+                        <img src="assets/images/products/skyper.png" alt="skyper" />
+                    </div>
+                    <div className="text_details">
+                        <div className="text_skyper">Skyper</div>
+                        <div className="text_paragraph2">Lorem ipsum dolor sit amet, consectetur adipiscing eiusmod tempor</div>
+                        <div className="text_amount">&#8377; 1500</div>
+                    </div>               
+                </div>
+
+                <div className="container_outerbox2">
+                    <div className="container_img2">
+                        <img src="assets/images/products/skyper.png" alt="skyper" />
+                    </div>
+                    <div className="text_details">
+                        <div className="text_skyper">Skyper</div>
+                        <div className="text_paragraph2">Lorem ipsum dolor sit amet, consectetur adipiscing eiusmod tempor</div>
+                        <div className="text_amount">&#8377; 1500</div>
+                    </div>               
+                </div> 
+
+                <div className="container_outerbox3">
+                    <div className="container_img2">
+                        <img src="assets/images/products/skyper.png" alt="skyper" />
+                    </div>
+                    <div className="text_details">
+                        <div className="text_skyper">Skyper</div>
+                        <div className="text_paragraph2">Lorem ipsum dolor sit amet, consectetur adipiscing eiusmod tempor</div>
+                        <div className="text_amount">&#8377; 1500</div>
+                    </div>               
+                </div> 
+
+            </div>
+        </div>
+    </Layout>   
+  );
+}
